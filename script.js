121616d3ea8309f54f28fd22fb607d9dd5bb11b3
Aydin Merchant <30785266+Aydinm1@users.noreply.github.com>
2025-10-03 00:26:26 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function selectCountry(event) {
+  event.preventDefault();
+
+  const countryname = document.getElementById("name").value;
+  const url = "https://restcountries.com/v3.1/name/";
+
+  fetch(url + countryname + "?fullText=true")
+    .then((raw) => raw.json())
+    .then((data) => {
+      document.getElementById("countryName").innerHTML = data[0].name.common;
+      const pop = data[0].population;
+      document.getElementById("population").innerHTML = numberWithCommas(pop);
+      document.getElementById("capital").innerHTML = data[0].capital;
+      document.getElementById("call").innerHTML = data[0].idd.root;
+      document.getElementById("time").innerHTML = data[0].region;
+      document.getElementById("img1").src = data[0].flags.png;
+    })
+    .catch((err) => {
+      console.error("Error fetching country data:", err);
+    });
+}
