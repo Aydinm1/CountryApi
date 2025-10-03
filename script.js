edf0feb277ea1462f228829238414a36e37a9edb
Aydin Merchant <30785266+Aydinm1@users.noreply.github.com>
2025-10-03 00:50:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-function selectCountry(){
-  event.preventDefault()
-var countryname = document.getElementById("name").value;
+function selectCountry() {
+  const countryname = document.getElementById("name").value;
+  const url = "https://restcountries.com/v3.1/name/";
 
- var url = 'https://restcountries.com/v3.1/name/';
-fetch(url + countryname + "?fullText=true")
-  .then(function(raw){
-  return raw.json();
-})
-  .then(function(data){
-  document.getElementById("countryName").innerHTML = (data[0].name.common);
-var pop = data[0].population;
-var pop2 = numberWithCommas(pop);
-document.getElementById("population").innerHTML = (pop2);
-document.getElementById("capital").innerHTML = (data[0].capital);
-document.getElementById("call").innerHTML = (data[0].idd.root);
-document.getElementById("time").innerHTML = (data[0].region);
-document.getElementById("img1").src = (data[0].flags.png);
-  
-});
+  fetch(url + countryname + "?fullText=true")
+    .then((raw) => raw.json())
+    .then((data) => {
+      if (!data || data.status === 404) {
+        document.getElementById("countryName").innerHTML = "Not found";
+        return;
+      }
 
-};
+      document.getElementById("countryName").innerHTML = data[0].name.common;
+      const pop = data[0].population;
+      document.getElementById("population").innerHTML = numberWithCommas(pop);
+      document.getElementById("capital").innerHTML = data[0].capital;
+      document.getElementById("call").innerHTML = data[0].idd.root;
+      document.getElementById("time").innerHTML = data[0].region;
+      document.getElementById("img1").src = data[0].flags.png;
+    })
+    .catch((err) => {
+      console.error("Error fetching country data:", err);
+    });
+}
